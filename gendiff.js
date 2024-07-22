aff0ba301fe52184869269c928268d778d223637
dp <petrishindmitry1997@gmail.com>
2024-07-22 10:57:50 +0300

    add format in option

--- gendiff.js
+++ gendiff.js
@@ -5,9 +5,11 @@ const program = new Command();
 
 program
   .name('gendiff')
-  .description('Compares two configuration files and shows the difference.')
+  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-V, --version', 'output the version number')
+  .option('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action();
