e298f7d5d432c3c814a931da52445844b1bf6011
dp <petrishindmitry1997@gmail.com>
2024-07-22 12:03:09 +0300

    add CI and tests

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { expect, test } from '@jest/globals';
+import fs from 'fs';
+import genDiff from '../src/gendiff-src.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
+
+test('flat json', () => {
+  const file1 = getFixturePath('flat1.json');
+  const file2 = getFixturePath('flat2.json');
+
+  expect(genDiff(file1, file2)).toEqual(readFile('result1.txt'));
+});
