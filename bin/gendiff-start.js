fe1c33330a0a62c47fafab54a966f397b1698396
dp <petrishindmitry1997@gmail.com>
2024-07-22 15:55:26 +0300

    fix #17

--- bin/gendiff-start.js
+++ bin/gendiff-start.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import genDiff from '../src/gendiff-src.js';
+import genDiff from '../src/gendiff.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2, type) => {
     console.log(genDiff(filepath1, filepath2, type.format));
-  });
+  })
 
 program.parse();
