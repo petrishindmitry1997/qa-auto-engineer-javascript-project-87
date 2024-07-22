4df8fc2c0007bf0036a463a18ce0e22dff0e2b8d
dp <petrishindmitry1997@gmail.com>
2024-07-22 10:50:54 +0300

    add commander/start

---
+++ gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows the difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action();
+
+program.parse();
