5c461e14947f0c4e8c7161cfbebc10380f43f598
dp <petrishindmitry1997@gmail.com>
2024-07-26 12:10:48 +0300

    test refactoring

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -8,20 +8,25 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'UTF-8');
 
-const testCases = [
-  ['flat1.json', 'flat2.json', 'expectedStyle.txt', undefined],
-  ['flat1.yml', 'flat2.yml', 'expectedStyle.txt', undefined],
-  ['flat1.yml', 'flat2.yml', 'expectedPlain.txt', 'plain'],
-  ['flat1.json', 'flat2.json', 'expectedJson.txt', 'json'],
-];
+describe('gendiff two files', () => {
+  const testCases = [
+    ['flat1.json', 'flat2.json', 'style', 'expectedStyle.txt'],
+    ['flat1.yml', 'flat2.yml', 'style', 'expectedStyle.txt'],
+    ['flat1.yml', 'flat2.yml', 'plain', 'expectedPlain.txt'],
+    ['flat1.json', 'flat2.json', 'json', 'expectedJson.txt'],
+  ];
 
-test.each(testCases)('gendiff test comparison between two files', (file1, file2, expected, format) => {
-  expect(genDiff(getFixturePath(file1), getFixturePath(file2), format)).toEqual(readFile(expected));
+  test.each(testCases)('comparison between two files', (file1, file2, format, expectedResult) => {
+    const filepath1 = getFixturePath(file1);
+    const filepath2 = getFixturePath(file2);
+
+    expect(genDiff(filepath1, filepath2, format)).toEqual(readFile(expectedResult));
+  });
 });
 
 test('gendiff unsupported file', () => {
-  const file1 = getFixturePath('flat1.json');
-  const file2 = getFixturePath('flat2.json');
+  const filepath1 = getFixturePath('flat1.json');
+  const filepath2 = getFixturePath('flat2.json');
 
-  expect(() => genDiff(file1, file2, 'unsupported file')).toThrow('Error');
+  expect(() => genDiff(filepath1, filepth2, 'unsupported file')).toThrow('Error');
 });
